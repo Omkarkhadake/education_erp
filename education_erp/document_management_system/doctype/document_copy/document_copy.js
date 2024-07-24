11c8fbe088eea8181fedbf1829f683c1783dd6dd
Omkarkhadake <omkarsatishkhadke132001@gmail.com>
2024-07-24 14:56:13 +0530

    dms chnages update

--- education_erp/document_management_system/doctype/document_copy/document_copy.js
+++ education_erp/document_management_system/doctype/document_copy/document_copy.js
@@ -1,8 +1,6 @@
-// Copyright (c) 2024, ppcrc and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2024, ppcrc and contributors
+// // For license information, please see license.txt
 
 // frappe.ui.form.on("Document Copy", {
-// 	refresh(frm) {
 
-// 	},
 // });
--- education_erp/document_management_system/doctype/document_revision/document_revision.js
+++ education_erp/document_management_system/doctype/document_revision/document_revision.js
@@ -1,8 +1,3 @@
 // Copyright (c) 2024, ppcrc and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Document Revision", {
-// 	refresh(frm) {
-
-// 	},
-// });

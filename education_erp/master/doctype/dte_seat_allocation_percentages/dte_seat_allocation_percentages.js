1e29adb3181792450ccbbfbd5307fffdaaa932ce
Omkarkhadake <omkarsatishkhadke132001@gmail.com>
2024-07-31 11:11:52 +0530

    IQSC

---
+++ education_erp/master/doctype/dte_seat_allocation_percentages/dte_seat_allocation_percentages.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ppcrc and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("DTE Seat Allocation Percentages", {
+// 	refresh(frm) {
+
+// 	},
+// });
